4c69f90be0ff214630157f65e8027395cc6fd11e
joebordes <joe@tsolucio.com>
2022-06-24 21:23:50 +0200

    fix corebos authorization header

--- javascript/npm/WSClientm.js
+++ javascript/npm/WSClientm.js
@@ -36,7 +36,7 @@ var fetchOptions = {
 	mode: 'cors',
 	headers: {
 		'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-		'corebos-authorization': _sessionid,
+		'corebos_authorization': _sessionid,
 	}
 };
 
